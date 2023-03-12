9f3715f0280e20397d3b8119f592920089840450
k-d-yu <dima_kul95@mail.ru>
2023-03-12 12:30:06 +0300

    HW_2

--- HW_flsk/main.py
+++ HW_flsk/main.py
@@ -1,27 +1,67 @@
-from flask import Flask, render_template, url_for
+import sqlite3
+import os
+from flask import Flask, render_template, url_for, g
+from FDateBase import FDateBase
 
+DATABASE = "/path/HW_flsk.db"
+DEBUG = True
+SECRET_KEY = "b4d6ad6dc4a0ba3eda5012d214e320c2ae17f5c6"
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(DATABASE=os.path.join(app.root_path, "HW_flsk.db"))
 
-menu = [{"name": "Первая страница", "url": "page_1"},
-        {"name": "Вторая страница", "url": "page_2"},
-        {"name": "Третья страница", "url": "page_3"}]
+
+def connect_db():
+    con = sqlite3.connect(app.config["DATABASE"])
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource("HW_sq_db.sql", "r") as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    if not hasattr(g, "link_db"):
+        g.link_db = connect_db()
+    return g.link_db
 
 
 @app.route("/")
-@app.route("/page_1")
 def page_1():
-    return render_template("page_1.html", title="Первая страница", menu=menu)
+    db = get_db()
+    dbase = FDateBase(db)
+    return render_template("page_1.html", title="Первая страница", menu=dbase.get_menu())
 
 
 @app.route("/page_2")
 def page_2():
-    return render_template("page_2.html", title="Вторая страница", menu=menu)
+    db = get_db()
+    dbase = FDateBase(db)
+    return render_template("page_2.html", title="Вторая страница", menu=dbase.get_menu())
 
 
 @app.route("/page_3")
 def page_3():
-    return render_template("page_3.html", title="Третья страница", menu=menu)
+    db = get_db()
+    dbase = FDateBase(db)
+    return render_template("page_3.html", title="Третья страница", menu=dbase.get_menu())
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "link.db"):
+        g.link_db.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html", title="Страница не найдена")
 
 
 if __name__ == "__main__":
